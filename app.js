e4d3ee7dee8c5c3cbc3d0f02a4da8775d2805651
jeoman <kremav1@naver.com>
2018-03-28 15:42:04 +0900

    BOARD

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = require('./config/express')();
+var passport = require('./config/passport')(app);
+
+var board = require('./routes/board')();
+app.use('/board', board);
+ 
+var index = require('./routes/index')(passport);
+app.use('/', index);
+
+app.listen(3009, function(){
+    console.log('Connected port 3009')
+});
